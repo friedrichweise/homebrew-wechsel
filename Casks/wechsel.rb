daeaa90ce288f1b9989eed304b07c1f12afe27f5
friedrichweise <account@weise.io>
2019-02-06 21:15:12 +0100

    update checksum

--- Casks/wechsel.rb
+++ Casks/wechsel.rb
@@ -1,6 +1,6 @@
 cask 'wechsel' do
   version '0.3.0'
-  sha256 '5f44cec482fef2a006bb3253e001a84a712e7b2799363d5826e648de6c787763'
+  sha256 'eb9528eb129765e969c20b45a28384794a0ee0f64b5afcada5818e74440bf8fb'
 
   url 'https://github.com/friedrichweise/wechsel/releases/download/v0.3-beta/wechsel_v0.3-beta.zip'
   appcast 'https://github.com/friedrichweise/wechsel/releases.atom'
