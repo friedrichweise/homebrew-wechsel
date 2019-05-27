18c98ca6c7697f7bedd9c96e7b9428e5b53333a3
friedrichweise <account@weise.io>
2019-05-27 12:20:59 +0200

    update sha checksum to match updatet file

--- Casks/wechsel.rb
+++ Casks/wechsel.rb
@@ -1,11 +1,11 @@
 cask 'wechsel' do
   version '0.4.0'
-  sha256 'a7df38f5dee25c8b8e27d70402b1385c3e4728d6d7548f18dddcdfcb0b99be87'
+  sha256 '8c6a7d5cfb95151408c1633142343816611f67dc17aadfac88342b071fe4e8a9'
 
   url 'https://github.com/friedrichweise/wechsel/releases/download/v0.4-beta/wechsel_v0.4-beta.zip'
   appcast 'https://github.com/friedrichweise/wechsel/releases.atom'
   name 'wechsel'
-  homepage 'https://github.com/friedrichweise/wechsel'
+  homepage 'https://wechsel.weise.io'
 
   app 'wechsel.app'
 end
