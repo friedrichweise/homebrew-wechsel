03d020bb9907d2ce4892f3f4b4e0861d78a4ca02
friedrichweise <account@weise.io>
2019-05-29 18:57:01 +0200

    version 1.0.1

--- Casks/wechsel.rb
+++ Casks/wechsel.rb
@@ -1,8 +1,8 @@
 cask 'wechsel' do
-  version '1.0.0'
-  sha256 '022d430258ffe671e59f7c3e727bf761a6263f4595ffe0471564e43013f17f0d'
+  version '1.0.1'
+  sha256 '37352e2eaea28c7021ffed50f59934185f5858bfbdac932f352efcda2b1d743a'
 
-  url 'https://github.com/friedrichweise/wechsel/releases/download/v1.0/wechsel_v1.0.zip'
+  url 'https://github.com/friedrichweise/wechsel/releases/download/v1.01/wechsel_v1.0.1.zip'
   appcast 'https://github.com/friedrichweise/wechsel/releases.atom'
   name 'wechsel'
   homepage 'https://wechsel.weise.io'
